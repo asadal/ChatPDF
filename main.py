3c6fe39b35495dc426205716387eaaa7233b6cf8
asadal <blaulaut@gmail.com>
2023-05-11 09:13:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 
 # Replace book.pdf with any pdf of your choice
-loader = UnstructuredPDFLoader("book.pdf")
+my_pdf = input("PDF 파일 경로를 입력하세요. 혹은 파일을 끌어다 여기에 놓으세요: ")
+loader = UnstructuredPDFLoader(my_pdf)
 pages = loader.load_and_split()
 embeddings = OpenAIEmbeddings()
 docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
 
 # Choose any query of your choice
-query = "Who is Rich Dad?"
-docs = docsearch.get_relevant_documents(query)
-chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-output = chain.run(input_documents=docs, question=query)
-print(output)
+answer = y
+while answer != "n":
+    question = input("무엇이든 물어보세요: ")
+    query = question  
+    docs = docsearch.get_relevant_documents(query)
+    chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+    output = chain.run(input_documents=docs, question=query)
+    print(output)
+    proceed = input("더 궁금한 점이 있나요? (y/n): ")
+    answer == proceed
+print("Cheers~! :)")
