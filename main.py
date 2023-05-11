966bb0c115b088f3d9be3c6521a7faf1e7a1813a
asadal <blaulaut@gmail.com>
2023-05-11 10:00:42 +0900

    Update main.py with streamlit app

--- main.py
+++ main.py
@@ -4,23 +4,37 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
+from streamlit as st
 
-# Replace book.pdf with any pdf of your choice
-my_pdf = input("PDF 파일 경로를 입력하세요. 혹은 파일을 끌어다 여기에 놓으세요: ")
-loader = UnstructuredPDFLoader(my_pdf)
-pages = loader.load_and_split()
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+def app():
+    st.set_page_config(page_title=Chat with PDF, page_icon="📄")
+    st.image("https://cdn.iconscout.com/icon/free/png-256/free-chat-2130787-1794829.png", width=150)
+    st.title("Chat with PDF")
+    st.subheader("Upload PDF and chat with it. Enjoy!")
+   
+    my_pdf = st.file_uploader("Upload PDF", type=["pdf"])
+    if my_pdf is not None:
+        loader = UnstructuredPDFLoader(my_pdf)
+        pages = loader.load_and_split()
+        embeddings = OpenAIEmbeddings()
+        docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+        answer = y
+        while answer != "n":
+            question = st.text_input("무엇이든 물어보세요.")
+            query = question
+            docs = docsearch.get_relevant_documents(query)
+            chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+            output = chain.run(input_documents=docs, question=query)
+            st.write(output)
+            proceed = st.text_input("더 궁금한 점이 있나요?")
+            if st.button("예, 물어볼게요."):
+                answer = 'y'
+            elif st.button("안, 물어볼게요."):
+                answer = 'n'
+            else:
+                pass
+        st.write("Cheers~! :-)")
+        st.end()
 
-# Choose any query of your choice
-answer = y
-while answer != "n":
-    question = input("무엇이든 물어보세요: ")
-    query = question  
-    docs = docsearch.get_relevant_documents(query)
-    chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-    output = chain.run(input_documents=docs, question=query)
-    print(output)
-    proceed = input("더 궁금한 점이 있나요? (y/n): ")
-    answer == proceed
-print("Cheers~! :)")
+if __name__ == "__main__":
+    app()
