3e5750f3b319b5a7abd173dd65b8049df37c3a56
asadal <blaulaut@gmail.com>
2023-05-11 12:45:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,47 @@ def app():
     st.image("https://cdn.iconscout.com/icon/free/png-256/free-chat-2130787-1794829.png", width=150)
     st.title("Chat with PDF")
     st.subheader("Upload PDF and chat with it. Enjoy!")
-   
+
+    # Get the OpenAI API key from the environment
+    openai_api_key = os.environ["OPENAI_API_KEY"]
+
+    # If the OpenAI API key is not set, raise an error
+    if openai_api_key is None:
+        raise ValueError("Did not find openai_api_key, please set it as an environment variable.")
+
+    # Upload a PDF file
     my_pdf = st.file_uploader("Upload PDF", type=["pdf"])
     if my_pdf is not None:
-        openai_api_key = st.secrets['openai']["OPENAI_API_KEY"]
+        # Load the PDF file
         loader = UnstructuredPDFLoader(my_pdf)
         pages = loader.load_and_split()
-        embeddings = OpenAIEmbeddings()
+
+        # Create an OpenAI embedding model
+        embeddings = OpenAIEmbeddings(openai_api_key)
+
+        # Create a Chroma document searcher
         docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
-        answer = y
-        while answer != "n":
+
+        # Start a chat loop
+        answer = "y"
+        while answer == "y":
+            # Get the user's question
             question = st.text_input("무엇이든 물어보세요.")
             query = question
+
+            # Get the relevant documents from the document searcher
             docs = docsearch.get_relevant_documents(query)
+
+            # Create a question-answering chain
             chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+
+            # Run the question-answering chain
             output = chain.run(input_documents=docs, question=query)
+
+            # Write the output to the chat
             st.write(output)
+
+            # Ask the user if they have any more questions
             proceed = st.text_input("더 궁금한 점이 있나요?")
             if st.button("예, 물어볼게요."):
                 answer = 'y'
