a7a33f00851bbd0fdb684959ea0da47487e29993
asadal <blaulaut@gmail.com>
2023-05-11 15:16:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def app():
     st.subheader("Upload PDF and chat with it. Enjoy!")
 
     # Get the OpenAI API key from the environment
-    openai_api_key = st.secrets['openai']["OPENAI_API_KEY"]
+    openai_api_key = st.secrets["openai_api_key"]
 
     # If the OpenAI API key is not set, raise an error
     if openai_api_key is None:
