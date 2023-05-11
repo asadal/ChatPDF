0e200b8649130edf2d78435caef0d522c8e1951c
asadal <blaulaut@gmail.com>
2023-05-11 16:27:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
